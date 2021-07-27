cf2ab65ef87624eb178d35d07522dbad756753c9
Marcio G B de Avellar <mgb.avellar@gmail.com>
2021-07-27 12:26:45 -0300

    Entity User e REST funcionando

---
+++ src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,69 @@
+package com.example.demo.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    // Atributos
+    private String id;
+    private String name;
+    private String email;
+
+    // Associações
+
+
+    // Construtores
+    public User () {
+
+    }
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters e Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // HashCode e Equals
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
