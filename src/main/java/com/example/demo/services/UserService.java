05db4d5e79dc08c0ffc56bbbc5592d9704442053
Marcio G B de Avellar <mgb.avellar@gmail.com>
2021-08-02 15:31:48 -0300

    Obtendo usuário por Id

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -2,10 +2,12 @@ package com.example.demo.services;
 
 import com.example.demo.domain.User;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +18,12 @@ public class UserService {
     public List<User> findAll() {
 
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+
+        //User user = repo.findOne(id); // Não funciona por causa nas mudanças no Spring Boot 1 -> 2
+        Optional<User> user = repo.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
