c63ca54c8a987eca39904af3ad126acc740debbc
Marcio G B de Avellar <mgb.avellar@gmail.com>
2021-08-04 12:09:16 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -37,4 +37,11 @@ public class UserService {
 
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
+
+    public void delete(String id) {
+
+        findById(id);
+        repo.deleteById(id);
+    }
+
 }
