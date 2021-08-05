a35b810888d353a80f750b505156da508d5b03b2
Marcio G B de Avellar <mgb.avellar@gmail.com>
2021-08-05 11:24:51 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/example/demo/config/Instantiation.java
+++ src/main/java/com/example/demo/config/Instantiation.java
@@ -1,28 +1,45 @@
 package com.example.demo.config;
 
+import com.example.demo.domain.Post;
 import com.example.demo.domain.User;
+import com.example.demo.repository.PostRepository;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
+    // Para salvar os usuários no banco de dados, preciso injetar o UserRepository aqui.
     @Autowired
     private UserRepository userRepository;
+
+    // Para salvar os posts no banco de dados, preciso injetar o PostRepository aqui também.
+    @Autowired
+    private PostRepository postRepository;
 
     @Override
     public void run(String... args) throws Exception {
 
+        // Para a aula 354: criando uma entidade Post
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+
+        // Para limpar qualquer coisa do banco para testarmos
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User rosi = new User(null, "Rosi Zila", "rosi@gmail.com");
         User alex = new User(null, "Alex Bombs", "alex@gmail.com");
         User neno = new User(null, "Neno Grey", "neno@gmail.com");
 
+        // Salvando no banco
         userRepository.saveAll(Arrays.asList(rosi, alex, neno));
         /*
         Aqui, tive o mesmo problema que outras pessoas do curso tiveram: em vez de
@@ -31,5 +48,10 @@ public class Instantiation implements CommandLineRunner {
         'userRepository.saveAll(Arrays.asList(rosi, alex, neno));'
          */
 
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", rosi);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", rosi);
+
+        // Salvando no banco
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
